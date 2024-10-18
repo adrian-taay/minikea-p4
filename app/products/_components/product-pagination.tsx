d488a2d1c26616c262316bc1646b9eaf56fde0bf
Adrian <adrian.tm@live.com>
2024-10-18 11:23:10 +0800

    fix: pagination

--- app/products/_components/product-pagination.tsx
+++ app/products/_components/product-pagination.tsx
@@ -3,6 +3,12 @@
 import clsx from "clsx";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
 import { useState } from "react";
+import {
+  MdChevronLeft,
+  MdChevronRight,
+  MdFirstPage,
+  MdLastPage,
+} from "react-icons/md";
 
 export default function ProductPagination({
   total,
@@ -38,40 +44,68 @@ export default function ProductPagination({
   const previousButton = (
     <span
       onClick={() => handleClickPage(String(Number(currentPage) - 1))}
-      className="cursor-pointer">
-      Prev
+      className="cursor-pointer hover:ring-1 px-2 ring-neutral-400 py-1.5"
+    >
+      <MdChevronLeft />
     </span>
   );
 
   const nextButton = (
     <span
       onClick={() => handleClickPage(String(Number(currentPage) + 1))}
-      className="cursor-pointer">
-      Next
+      className="cursor-pointer hover:ring-1 px-2 ring-neutral-400 py-1.5"
+    >
+      <MdChevronRight />
+    </span>
+  );
+
+  const firstPage = (
+    <span
+      onClick={() => handleClickPage(String(1))}
+      className="cursor-pointer hover:ring-1 px-2 ring-neutral-400 py-1.5"
+    >
+      <MdFirstPage />
+    </span>
+  );
+
+  const lastPage = (
+    <span
+      onClick={() => handleClickPage(String(totalPages))}
+      className="cursor-pointer hover:ring-1 px-2 ring-neutral-400 py-1.5"
+    >
+      <MdLastPage />
     </span>
   );
 
   const pageButtons = [...Array(totalPages)].map((_, index) => {
     index += 1;
+
+    if (index > Number(currentPage) + 2 || index < Number(currentPage) - 2)
+      return;
+
     return (
       <span
         key={index}
         onClick={() => handleClickPage(String(index))}
         className={clsx(
           "cursor-pointer",
+          "hover:ring-1 px-2 ring-neutral-400",
           currentPage === String(index) &&
-            "font-bold ring-1 px-2 ring-neutral-800 rounded-md"
-        )}>
-        {index}
+            "font-bold ring-1 px-2 ring-neutral-800"
+        )}
+      >
+        {index < 10 ? `0${index}` : index}
       </span>
     );
   });
 
   return (
-    <div className="flex gap-4 text-lg">
+    <div className="flex gap-4 items-center">
+      {firstPage}
       {previousButton}
       {pageButtons}
       {nextButton}
+      {lastPage}
     </div>
   );
 }
