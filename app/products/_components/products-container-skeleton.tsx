598c7f57e0de8c4decb0d4f02bb13f9f769a4cc9
Adrian <adrian.tm@live.com>
2024-11-05 15:54:09 +0800

    fix: added suspense on /products/loading.tsx

--- app/products/_components/products-container-skeleton.tsx
+++ app/products/_components/products-container-skeleton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ProductSort from "./product-sort";
 import ProductLimit from "./product-limit";
 import ProductSkeleton from "./product-skeleton";
@@ -9,8 +9,10 @@ export default function ProductsContainerSkeleton() {
       <div className="flex flex-col-reverse lg:flex-row lg:items-center justify-between mb-4 gap-3">
         <p className="bg-neutral-300 animate-pulse h-6 w-3/12 rounded-full"></p>
         <div className="flex flex-col md:flex-row gap-3">
-          <ProductSort />
-          <ProductLimit />
+          <Suspense>
+            <ProductSort />
+            <ProductLimit />
+          </Suspense>
         </div>
       </div>
       <div className="grid gap-8 md:grid-cols-2 md:gap-4 py-4 lg:grid-cols-4 xl:grid-cols-5">
